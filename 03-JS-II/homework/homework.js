7943474788a9a59fad54d2802242e9000bb9a8f1
Regenerik <regenerik@gmail.com>
2022-04-04 21:45:46 -0300

    test

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,7 +5,7 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-
+//hola
 if ( x > y ){
   return x;
 }else if ( y > x ){
